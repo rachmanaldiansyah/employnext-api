adc89925441c6bd0f20729359a000a9277091146
rachmanaldiansyah <rachmanaldiansyah03@gmail.com>
2024-12-30 08:30:26 +0700

    Add authentication views and templates for login and signup

--- apps/cores/views.py
+++ apps/cores/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
-    return render(request, 'auth/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # log the user in
+            login(request, user)
+            return redirect('frontpage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/signup.html', {'form': form})
 
 def frontpage(request):
     return render(request, 'cores/frontpage.html')
--- employnext_api/urls.py
+++ employnext_api/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views
 
 from apps.cores.views import frontpage, signup
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('admin/', admin.site.urls),
 ]
